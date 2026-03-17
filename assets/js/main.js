5f31ca09160c6b18a002e9af365d9972e4bf0824
pauloearmani <paulo.armani@aluno.ufop.edu.br>
2026-03-17 17:22:45 -0300

    adiciona logo real na navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,13 +183,7 @@ function injectNavbar(activePage) {
 
   nav.innerHTML = `
     <a href="index.html" class="nav-logo">
-      <div class="logo-icon">
-        <svg width="22" height="22" viewBox="0 0 24 24" fill="none"
-             stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/>
-          <polyline points="9 22 9 12 15 12 15 22"/>
-        </svg>
-      </div>
+      <img src="assets/images/logo.png" alt="Grupo AL Construções" class="nav-logo-img"/>
       Grupo <span>AL Construções</span>
     </a>
 
